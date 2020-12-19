3fa520ae6c6cb605438fca9d304851dda1aca0ca
zweack <jeet88833@gmail.com>
2020-12-19 16:59:41 +0530

    function for getting matches

---
+++ get_score.py
@@ -0,0 +1,28 @@
+import re
+import logging
+import requests
+
+from bs4 import BeautifulSoup
+
+from utility import logAndExit
+
+def getCurrentMatches(url):
+    try:
+        result = requests.get(url)
+    except:
+        logAndExit()
+
+    soup = BeautifulSoup(result.text, "lxml")
+    xml = soup.find_all("item")
+
+    matches = list(map(lambda item: re.sub(r'\s+', " ", re.sub('[^A-Za-z ]+', '', item.title.text)), xml))
+    return (matches, xml)
+
+
+def getMatchID(match, xml):
+    guid = xml[match].guid.text
+    matchID = re.search(r'\d+', guid).group()
+    return matchID
+
+
+
