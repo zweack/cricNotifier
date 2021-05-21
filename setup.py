224d343c1ea6451251347a13d92be3c46d110fef
Jeet Jain <Jeet88833@gmail.com>
2021-05-21 13:04:27 +0530

    python package bugfix

--- setup.py
+++ setup.py
@@ -7,14 +7,16 @@ def readme():
 
 setuptools.setup(
     name='cricNotifier',
-    version=1.0,
+    version="1.0.3",
     description='Cross platform cricket score notifications',
     long_description=readme(),
+    long_description_content_type='text/markdown',
     url='https://github.com/zweack/cricNotifier',
     author='Jeet Jain',
     author_email='jeet88833@gmail.com',
     license='MIT',
     packages=setuptools.find_packages(include=['app', 'app.*']),
+    include_package_data=True,
     install_requires=[
         'requests',
         'beautifulsoup4',
@@ -27,6 +29,5 @@ setuptools.setup(
         'console_scripts': [
             'cricNotifier = app.main:main'
         ]
-    },
-    zip_safe=False
+    }
 )
