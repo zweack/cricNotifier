f65935b0511988dabac8749f16b1d2250de04dde
Jeet Jain <Jeet88833@gmail.com>
2021-06-19 23:47:25 +0530

    scoreboard imporvements

--- test/test_scoreboard.py
+++ test/test_scoreboard.py
@@ -11,7 +11,7 @@ logger = logging.getLogger(__name__)
 
 def test_getMatchID():
     logger.info("starting test using pytest")
-    matches, xml = getCurrentMatches(
+    _, xml = getCurrentMatches(
         url="http://static.cricinfo.com/rss/livescores.xml")
     matchID = getMatchID(1, xml)
     assert isinstance(matchID, str)
